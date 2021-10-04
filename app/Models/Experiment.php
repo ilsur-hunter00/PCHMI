e17ceec7b3f36eb14c042d1bc5e0554892bb71f8
Laziz <lazizabdullaev23@gmail.com>
2021-10-04 22:18:44 +0300

    add model

---
+++ app/Models/Experiment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Experiment extends Model
+{
+    protected $table = 'experiments';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $fillable = [
+        'name',
+        'num_count_1',
+        'num_count_2',
+        'pict_count_1',
+        'pict_count_2',
+        'bold_count_1',
+        'bold_count_2',
+        'cursive_count_1',
+        'cursive_count_2'
+    ];
+}
